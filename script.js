4235b822498e701e5c4bb3db6cf77816d0acdf30
shreshhhth <76917832+shreshhhth@users.noreply.github.com>
2024-02-22 18:08:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -122,28 +122,34 @@ function circleSkew() {
 document.querySelectorAll(".elem").forEach(function (elem) {
     var rotate = 0;
     var diffrot = 0;
-
+  
     elem.addEventListener("mouseleave", function (dets) {
-        gsap.to(elem.querySelector("img"), {
-            opacity: 0,
-            ease: Power3,
-            duration: 0.5,
-        });
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
     });
-
+  
     elem.addEventListener("mousemove", function (dets) {
-        var diff = dets.clientY - elem.getBoundingClientRect().top;
-        diffrot = dets.clientX - rotate;
-        rotate = dets.clientX;
-        gsap.to(elem.querySelector("img"), {
-            opacity: 1,
-            ease: Power3,
-            top: diff,
-            left: dets.clientX,
-            rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
-        });
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
     });
-});
+  });
+
+
+
+
+
+
 
 
 
@@ -153,5 +159,5 @@ document.querySelectorAll(".elem").forEach(function (elem) {
 circleMouseFollower();
 firstPageAnim();
 circleSkew();
-
+animCircle()
 
